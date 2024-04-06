5705c6146c49b21830263bfd44f7d0c56f72faae
Anton Nefedenkov <anton@koala.health>
2024-04-06 13:46:34 -0400

    revert busted refactor

--- lib/kickbox/http_client/response_handler.rb
+++ lib/kickbox/http_client/response_handler.rb
@@ -1,22 +1,20 @@
 module Kickbox
-
   module HttpClient
-
     # ResponseHandler takes care of decoding the response body into suitable type
     class ResponseHandler
 
-      def self.get_body(env)
-        type = env.response_headers["content-type"] || ''
+       def self.get_body(env)
+         type = env.response_headers["content-type"] || ''
+         body = env.body
 
-        # Response body is in JSON
-        type.include?("json") ?
-          return JSON.parse(body) :
-          return env.body
+         # Response body is in JSON
+         if type.include?("json")
+           body = JSON.parse body
+         end
 
-      end
+         return body
+       end
 
-    end
-
+     end
   end
-
 end
